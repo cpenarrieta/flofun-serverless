b8c5d181a6da719622d09f4886029777cad3be1d
Cristian Penarrieta <cristian.penarrieta@gmail.com>
2017-05-04 22:24:33 -0700

    refactor createUser

--- functions/createUser.js
+++ functions/createUser.js
@@ -1,18 +1,21 @@
 const admin = require('firebase-admin')
 
 module.exports = function(req, res) {
-  if (!req.body.phone) {
+  if (!req.body.uid) {
     return res.status(422).send({ error: 'bad input' })
   }
 
-  const phone = String(req.body.phone).replace(/[^\d]/g, '')
+  const uid = String(req.body.uid).replace(/[^\d]/g, '')
 
-  admin.auth().getUser(phone)
-    .then(userRecord => {
-      res.send({ success: true })
-    })
+  admin.auth().getUser(uid)
+    .then(userRecord => saveRecord(uid, req.body, res))
     .catch(error => {
-      admin.auth().createUser({ uid: phone })
-        .then(user => res.send({ success: true }))
+      admin.auth().createUser({ uid: uid })
+        .then(user => saveRecord(uid, req.body, res))
     })
+}
+
+function saveRecord(uid, user, res) {
+  admin.database().ref(`users/${uid}`)
+    .update({ source: user.source, name: user.name || '' }, () => res.send({ success: true }))
 }
